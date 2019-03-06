92e0653655edd60116586779c1c306dccbb1a947
davidsth <1936038+davidsth@users.noreply.github.com>
2019-03-05 20:51:43 -0500

    Added new annotation Config

--- annotation/build.gradle.kts
+++ annotation/build.gradle.kts
@@ -7,5 +7,5 @@ plugins {
 }
 
 dependencies {
-    implementation(kotlin("stdlib-jdk8"))
+    implementation(kotlin("stdlib"))
 }
---
+++ annotation/main/kotlin/config/annotation/Config.kt
@@ -0,0 +1,5 @@
+package config.annotation
+
+@Retention(AnnotationRetention.RUNTIME)
+@Target(AnnotationTarget.CLASS)
+annotation class Config
