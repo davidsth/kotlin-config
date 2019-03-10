294c1afec139b5fb54e0710103dd1b8031197e07
davidsth <1936038+davidsth@users.noreply.github.com>
2019-03-10 16:56:55 -0400

    Updated gradle build for processor

--- processor/build.gradle.kts
+++ processor/build.gradle.kts
@@ -1,4 +1,8 @@
 import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+val mockkVersion = "1.9"
+val spekVersion = "2.0.0"
 
 group = "dev.davidsth"
 version = "1.0-SNAPSHOT"
@@ -20,17 +24,40 @@ dependencies {
     implementation(kotlin("stdlib-jdk8"))
 
     implementation(project(":annotation"))
+
+    implementation(kotlin("reflect"))
     implementation("com.google.auto.service:auto-service:1.0-rc2")
     implementation("com.squareup:kotlinpoet:0.7.0")
 
     //test
-    testImplementation("org.junit.jupiter:junit-jupiter:5.4.0")
+    testImplementation("org.spekframework.spek2:spek-dsl-jvm:$spekVersion") {
+        exclude("org.jetbrains.kotlin")
+    }
+    testRuntimeOnly("org.spekframework.spek2:spek-runner-junit5:$spekVersion") {
+        exclude("org.junit.platform")
+        exclude("org.jetbrains.kotlin")
+    }
+    testCompile("org.junit.platform:junit-platform-engine:1.3.1")
+    testImplementation("io.mockk:mockk:$mockkVersion")
+    //mock lib
+    testCompile("org.mockito:mockito-inline:2.24.5")
+    // expect matchers
+    testCompile("net.oddpoet:kotlin-expect:1.2.1")
+
 }
 
-tasks.withType<Test> {
-    useJUnitPlatform()
-    testLogging {
-        events("passed", "skipped", "failed")
+tasks {
+    withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+
+    withType<Test> {
+        useJUnitPlatform {
+            includeEngines("spek2")
+        }
+        testLogging {
+            events("passed", "skipped", "failed")
+        }
     }
 }
 
